2a1b4f4d619cc06b107b886301c54a1114021b5d
Maxim Zebzeev <maksimzeb@yandex.ru>
2020-12-23 12:25:26 +0500

    added let

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img1 = document.getElementById("myImg1");
-var img2 = document.getElementById("myImg2");
-var img3 = document.getElementById("myImg3");
-var img4 = document.getElementById("myImg4");
-var img5 = document.getElementById("myImg5");
-var img6 = document.getElementById("myImg6");
-var img7 = document.getElementById("myImg7");
+let img1 = document.getElementById("myImg1");
+let img2 = document.getElementById("myImg2");
+let img3 = document.getElementById("myImg3");
+let img4 = document.getElementById("myImg4");
+let img5 = document.getElementById("myImg5");
+let img6 = document.getElementById("myImg6");
+let img7 = document.getElementById("myImg7");
 
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
 img1.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
@@ -56,14 +56,14 @@ img7.onclick = function() {
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 // Get the <a> element that close the modal in area empaty
-var areaempaty = document.getElementsByClassName("popup__area")[0];
+let areaempaty = document.getElementsByClassName("popup__area")[0];
 
 areaempaty.onclick = function() {
 	modal.style.display = "none";
