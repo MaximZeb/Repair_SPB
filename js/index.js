aad4f4f1e5fbbbe8028344c968866070c3f6d634
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-01-10 10:31:08 +0500

    added

--- js/index.js
+++ js/index.js
@@ -68,3 +68,12 @@ let areaempaty = document.getElementsByClassName("popup__area")[0];
 areaempaty.onclick = function() {
 	modal.style.display = "none";
 }
+// неоновый скрипт
+let progress = document.getElementById('progressBar');
+
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = function() {
+	let progressHeight = (window.pageYOffset / totalHeight) * 100;
+	progress.style.height = progressHeight +"%";
+}
